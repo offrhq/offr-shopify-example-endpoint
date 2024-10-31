11fdad79831396226ec350366efd5e172569921d
Jonathan Stanley <jonathan@bristleconeweb.com>
2024-10-31 08:17:32 -0400

    chore: add try catch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 Deno.serve(async (req) => {
-  const formData = await req.formData();
-  console.log(req, formData);
+  try {
+    const formData = await req.formData();
+    console.log(req, formData);
+  } catch (error) {
+    console.warn(error);
+  }
 
   return new Response(JSON.stringify({ example: "hello world" }), {
     headers: { "Content-Type": "application/json" },
