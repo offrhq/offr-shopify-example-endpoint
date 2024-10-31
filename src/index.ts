d45217531b6186f49dd5f3babe48fd4a05e14557
Jonathan Stanley <jonathan@bristleconeweb.com>
2024-10-31 08:27:57 -0400

    feat: limit service to a path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-Deno.serve(async (req) => {
+Deno.serve({ path: "f7b9506c-a635-4aa2-81b2-939d4b7debd7" }, async (req) => {
   try {
     const formData = await req.formData();
     console.log(req, formData);
